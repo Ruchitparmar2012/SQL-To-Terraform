67b30bea11f3a138fbc21f4bad1f0e0dd7c5c7a7
Ruchit Parmar <97833164+Ruchitparmar2012@users.noreply.github.com>
2024-02-14 14:06:30 +0530

    Update Snowflake Stored Procedure to python config.py

--- All Snowflake To Python Config  File/Snowflake Stored Procedure to python config.py
+++ All Snowflake To Python Config  File/Snowflake Stored Procedure to python config.py
@@ -6,7 +6,7 @@ import snowflake.connector
 
 # Replace these with your Snowflake account credentials and connection details
 account = ''  # Replace with your Snowflake account URL
-warehouse = ''
+warehouse = 'DEMO_WH'
 database = ''
 schema = ''
 username = ''  # Replace with your Snowflake username
@@ -35,19 +35,29 @@ conn = snowflake.connector.connect(
 
 # Create a cursor to execute SQL queries
 cursor = conn.cursor()
+# pass your scehma here all 
+schemas = ['ACTIVE_DIRECTORY','ADMINPAYROLL','ALAYACARE','AMERIHEALTH','ASR','AXXESS','BI_REPOSITORY','CCSI','CENTENE',
+'CLEARCARE','COSTALSYNCDATA','DATAFLEXSYNCDATA','DATA_CLEANSING','DATA_VAULT','DEVERO','DOCEBO','EMPEONEDISON','EMPEONPREFERRED','EMPLOYEESETUP',
+'GENERATIONSALLIANCE','GPSYNCDATA','HAHUSERS','HAH_APP','HAH_REPORTING','HHAEXCHANGEEDISON','HHAEXCHANGEOPENSYSTEMS','HHAEXCHANGEOSHAH',
+'HHAEXCHANGEPREFERRED','JAZZHR','LANDING_8X8','MATRIXCARE','MATRIXCARE_FULLLOAD','MOLINA','PARADOX','PAYLOCITY',
+'PAYOR_CONTRACT_UI','PUBLIC','QUALTRICS_SURVEYS','RISKONNECT','SANDATAEXCHANGE','SANDATAIMPORT','TRUSTPOINTDATA','TRUSTPOINTDATA_HIST',
+'VIVENTIUM','WORKDAY','ZENDESK']
+
 
 # Query Snowflake to get a list of procedures in the specified database and schema
-procedure_query = f'''
-    SELECT procedure_name, ARGUMENT_SIGNATURE
+
+procedures = []
+for schema in schemas:
+# Execute the query to get the list of procedures
+    procedure_query = f'''
+    SELECT (procedure_catalog || '.' || procedure_schema || '.' || procedure_name) procedure_name, ARGUMENT_SIGNATURE
     FROM information_schema.procedures
     WHERE procedure_schema = '{schema}' AND procedure_catalog = '{database}'
-'''
-
-# Execute the query to get the list of procedures
-cursor.execute(procedure_query)
-
-# Fetch the results
-procedures = cursor.fetchall()
+    '''
+    cursor.execute(procedure_query)
+    procedure = cursor.fetchall()
+    procedures.extend(procedure)
+    
 
 # Close the cursor and connection when done with the procedure query
 cursor.close()
@@ -67,44 +77,45 @@ if procedures:
         if '()' not in procedure_info[1]:
             procedure_details = extract_type(procedure_info[1])
             #print(procedure_details, '\n')
-            ddl_query = f'''SELECT GET_DDL('PROCEDURE', '{database}.{schema}.{procedure_name}({procedure_details})')'''
+            ddl_query = f'''SELECT GET_DDL('PROCEDURE', '{procedure_name}({procedure_details})',true)'''
             #print(ddl_query, '\n')
         else:
-            ddl_query = f'''SELECT GET_DDL('PROCEDURE', '{database}.{schema}.{procedure_name}()')'''
+            ddl_query = f'''SELECT GET_DDL('PROCEDURE', '{procedure_name}()',true)'''
             #print(ddl_query, '\n')
 
         # Construct the fully qualified procedure name
-        fully_qualified_procedure_name = f'{database}.{schema}.{procedure_name}'
+        fully_qualified_procedure_name = f'{procedure_name}'
+        try:
+            # Query to retrieve the DDL statement for the procedure
+
+            # Create a new cursor for the DDL query
+            cursor = conn.cursor()
+
+            # Execute the DDL query
+            cursor.execute(ddl_query)
+
+            # Fetch the DDL statement
+            ddl_statement = cursor.fetchone()[0]
+
+            # Modify the DDL statement to include the database and schema names
+            modified_ddl_statement = ddl_statement.replace(f'CREATE OR REPLACE PROCEDURE "{procedure_name}"',
+                                                          f'CREATE OR REPLACE PROCEDURE {fully_qualified_procedure_name}')
+
+            # Remove line breaks and extra spaces
+            modified_ddl_statement = modified_ddl_statement.replace('\r\n', '\n').replace('\xa0', ' ').replace('\u2003', ' ').replace("''", "'")
         
-        # Query to retrieve the DDL statement for the procedure
+            # Step 2: Create a .sql file and write the DDL statement to it
+            sql_file_name = f"{procedure_dir}/{fully_qualified_procedure_name}.sql"
+            with open(sql_file_name, 'w') as sql_file:
+                sql_file.write(modified_ddl_statement.encode("utf-8").decode("utf-8"))
 
-        # Create a new cursor for the DDL query
-        cursor = conn.cursor()
+            # Step 3: Print the file name (database.schema.procedure name)
+            print(f"Generated SQL file: ", sql_file_name)
 
-        # Execute the DDL query
-        cursor.execute(ddl_query)
-
-        # Fetch the DDL statement
-        ddl_statement = cursor.fetchone()[0]
-
-        # Modify the DDL statement to include the database and schema names
-        modified_ddl_statement = ddl_statement.replace(f'CREATE OR REPLACE PROCEDURE "{procedure_name}"',
-                                                      f'CREATE OR REPLACE PROCEDURE {fully_qualified_procedure_name}')
-
-         # Remove line breaks and extra spaces
-        # modified_ddl_statement = modified_ddl_statement.replace('\r\n', '\n') 
-        modified_ddl_statement = modified_ddl_statement.replace('\r\n', '\n').replace('\xa0', ' ').replace('\u2003', ' ').replace("''", "'")
-        
-        # Step 2: Create a .sql file and write the DDL statement to it
-        sql_file_name = f"{procedure_dir}/{fully_qualified_procedure_name}.sql"
-        with open(sql_file_name, 'w') as sql_file:
-            sql_file.write(modified_ddl_statement)
-
-        # Step 3: Print the file name (database.schema.procedure name)
-        print(f"Generated SQL file: {sql_file_name}")
-
-        # Close the cursor for the DDL query
-        cursor.close()
+            # Close the cursor for the DDL query
+            cursor.close()
+        except snowflake.connector.errors.ProgrammingError as e:
+            print('Procedure does not exist or not authorized ', fully_qualified_procedure_name)
 else:
     print(f"No procedures found in the specified schema '{schema}' in database '{database}'.")
 
